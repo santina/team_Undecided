bbe6c1c619ea65339e146872ed4c4210ecd0bf84
Arjun Baghela <arjunsbaghela@gmail.com>
2017-02-04 22:25:33 -0800

    init commit. Try to download data from GEO.

---
+++ downloadData.R
@@ -0,0 +1,13 @@
+# Download the GEOQuery package. Using bioconductor to download. 
+# I had to update a tonne of packages to do this. 
+
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite("GEOquery")
+library(GEOquery)
+
+data <- getGEO("GSE85568")
+
+# OK so the above command does not work on my computer. I keep getting and error. 
+# Error in download.file(myurl, destfile, mode = mode, quiet = TRUE, method = getOption("download.file.method.GEOquery")) : 
+# cannot open URL 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?targ=self&acc=GPL11154&form=text&view=full'
